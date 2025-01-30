551dda08d9f52be1a6f686a6589b888b69ac16fd
rydalund <54313809+rydalund@users.noreply.github.com>
2025-01-30 07:54:11 +0100

    Initial commit

---
+++ src/main/java/com/example/text_drive/TextDriveApplication.java
@@ -0,0 +1,13 @@
+package com.example.text_drive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/text_drive/TextDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.text_drive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
